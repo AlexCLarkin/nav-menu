02692eef6b3141e99382817a8b9ebcd6e78fed72
Alex Larkin <alex@blendcommerce.com>
2022-05-18 14:41:41 +0100

    added dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { Page } from "@shopify/polaris";
+import { Page, Card, EmptyState } from "@shopify/polaris";
 
 const Dashboard = () => {
   return (
     <Page>
-      <h1>Dashboard</h1>
+      <div className="ui grid ">
+        <div className="center-paneln">
+          <Card>
+            <div className="center-card">
+              <Card.Section>
+                <EmptyState
+                  heading="Create new Showroom"
+                  action={{ content: "Choose page" }}
+                ></EmptyState>
+              </Card.Section>
+            </div>
+          </Card>
+        </div>
+        <div className="side-panel">
+          <Card>
+            <div className="side-card">
+              <Card.Section>
+                <h1>Side Panel</h1>
+              </Card.Section>
+            </div>
+          </Card>
+        </div>
+      </div>
     </Page>
   );
 };
